d761512aeee350916e4ed92173fa6283bb25cfdb
Guilherme Silva <guilhermefrrs@gmail.com>
2018-03-17 16:45:19 -0300

    Add problema1

---
+++ Atividade IV/problema1.hs
@@ -0,0 +1,12 @@
+potencia :: Int -> Int -> Int
+potencia x y  | (y == 1) =  x
+              |  otherwise = x * (potencia x (y - 1))
+
+main = do 
+  putStrLn "Primeiro numero:"
+  a <- getLine
+  putStrLn "Segundo numero:"
+  b <- getLine
+  let x = (read a :: Int)
+  let y = (read b :: Int)
+  print (potencia x y)
